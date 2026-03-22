888fc68a1f13ffcecb1931556be117c580d015f8
shopping <2313025041@students.unila.ac.id>
2026-03-22 17:35:31 +0700

    Prepare public deployment with Docker and env-based DB config

--- config.php
+++ config.php
@@ -1,13 +1,15 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "123223";
-$dbname = "db_kasir";
+$host = getenv('DB_HOST') ?: "localhost";
+$username = getenv('DB_USER') ?: "root";
+$password = getenv('DB_PASS') ?: "123223";
+$dbname = getenv('DB_NAME') ?: "db_kasir";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 if (!$conn){
         die("Connection Failed:".mysqli_connect_error());
     }
+
+mysqli_set_charset($conn, 'utf8mb4');
     
 date_default_timezone_set('Asia/Jakarta');   
 ?>
